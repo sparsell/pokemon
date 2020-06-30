724bd12b20c8afbc993571953edb8bd62b3d6d12
sparsell <hello@sparsell.com>
2020-06-30 14:13:25 -0400

    cli flow

--- lib/pokemon/cli.rb
+++ lib/pokemon/cli.rb
@@ -1,43 +1,41 @@
-require 'pry'
 
 module Pokemon
     class Pokemon::CLI
     
         def start
             get_user_name
+            list_pokemon
             choose_pokemon
-            validate_input(input)
 
         end
     end 
 end
 
-#start
-    #until the user selects 'exit'
-    # list 30 pokemon
-    # ask user for choice (1-30 or exit)
-    # validate input is w/in 1-30 and nothing else (except 'exit')
-    # retreive Pokemon data according to user # input
-    # once user types 'exit', quit program
-
         def get_user_name
+            puts ""
+            puts ""
+            puts ""
+            puts "Welcome to the Pokemon Library!"
+            puts ""
+            puts ""
+            puts "-----------------------------------"
             puts "What is your name, Pokemon expert?"
-            user_name = gets.chomp
-            puts "Welcome to the Pokemon library, #{user_name}!"
-            
+            @user_name = gets.chomp
+            puts "-----------------------------------" 
         end
         
-        def choose_pokemon
-            puts "Which Pokemon would you like to know more about? Choose a number between 1 - 30:"
-            @input = gets.chomp.to_i
-            pokemon = validate_input(input)
-            binding.pry
+        def list_pokemon
+            puts "-----------------------------------"
+            puts "Which Pokemon would you like to know more about, #{@user_name}?"
+            input = gets.chomp.to_i
+            Pokemon::API.get_name(input)
+
         end
 
-        def validate_input(input)
-            if @input > 30 || input < 0
+        def choose_pokemon(input)
+            if input > 30 || input < 0
                 puts "Please enter a number from 1 - 30"
-                @input = gets.chomp.to_i
+                input = gets.chomp.to_i
                 
             end
             input
